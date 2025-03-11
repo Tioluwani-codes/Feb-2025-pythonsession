ba842664e7c9947c1328fe24e99bf5207ad1c688
tioluwani-codes <toluwaniosho18@gmail.com>
2025-03-11 10:04:57 +0100

    first commit

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+# a = 18 # int dataType
+# masculine # string dataType
+# c = 2.24 # float dataType
+
+# List dataType
+languages = ["python", "HTML", "CSS", 3, 5.7, True]
+# print("languages")
+print(languages[5])
+print(languages[-4])
+print(languages[0:3])
