e5e0fbf3581ba783e0e4b8a58f0cf08480271a58
tioluwani-codes <toluwaniosho18@gmail.com>
2025-03-17 09:13:35 +0100

    first code challenge

---
+++ intro.py
@@ -0,0 +1,11 @@
+# variables hold data
+age = 25            # int (integer DateType)
+name = "Victor"       # str (string DateType)
+height = 5.9        # float (decimal)
+is_student = True   # bool (boolean)
+
+# printing out the variables
+print("Name:", name)                    #output: Name: Victor
+print("Age:", age)                      #output: Age: 25
+print("Height:", height)                #output: Height: 5.9
+print("Is a Student:", is_student)      #output: Is a Student: True
